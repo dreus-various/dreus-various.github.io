bbbe11e2906160f4b63f7816fc9dcc0830688bf4
dreus-various <wheig23@gmail.com>
2022-04-06 13:04:55 +0200

    Remove token

--- src/app/intereceptor/ErrorInterceptor.ts
+++ src/app/intereceptor/ErrorInterceptor.ts
@@ -2,16 +2,18 @@ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/com
 import {Injectable} from "@angular/core";
 import {catchError, EMPTY, Observable, tap} from "rxjs";
 import {Router} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private cookie: CookieService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError(err => {
+        this.cookie.delete('spotify_token');
         this.router.navigate(['/'])
         return EMPTY;
       })
